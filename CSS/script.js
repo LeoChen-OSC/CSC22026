62e66156a3608e623d0e52c3d737761fd7d247cc
LeoChen-OSC <st25246@ormiston.school.nz>
2026-03-04 13:55:07 +1300

    added swiper 2

--- CSS/script.js
+++ CSS/script.js
@@ -1,3 +1,53 @@
-new Swiper("#slide2",{
-    effect: "fade"
+new Swiper("#swiper-1", {
+
+    effect: "fade", //cube, fade, coverflow, flip
+    slidesPerView: "auto", //number of slides per view
+    centeredSlides: true, //center the active slide
+   
+
+    navigation: {
+        nextEl: ".swiper-button-next", //previous and next buttons
+        prevEl: ".swiper-button-prev",
+    },
+
+    // freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: false,
+
+        // type: "progressbar",
+        // type:"fraction",
+
+    },
+    loop: true,
+    autoplay: { //automatic plays the swiper
+       delay: 2000, //delay time
+    },
+})
+
+new Swiper("#swiper-2", {
+
+    effect: "fade", //cube, fade, coverflow, flip
+    slidesPerView: "auto", //number of slides per view
+    centeredSlides: true, //center the active slide
+   
+
+    navigation: {
+        nextEl: ".swiper-button-next", //previous and next buttons
+        prevEl: ".swiper-button-prev",
+    },
+
+    // freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: false,
+
+        // type: "progressbar",
+        // type:"fraction",
+
+    },
+    loop: true,
+    autoplay: { //automatic plays the swiper
+       delay: 2000, //delay time
+    },
 })

78494b0324c3c9d309cbcc305a729dcab76ef21e
LeoChen-OSC <st25246@ormiston.school.nz>
2026-03-06 14:02:32 +1300

    addded some button whicch does various stuff

--- CSS/script.js
+++ CSS/script.js
@@ -1,3 +1,32 @@
+let green=0;
+
+function myFunction() {
+    if (confirm("ボタンを選択してください")) {
+        alert("サンプルテキスト パイナップルは好きではありません");
+    }
+    else{
+        alert("chicken nuggets");
+        window.location.href = "nevergonnagiveyouup.html";
+        // redirects the user to a new page when they click the button
+    }
+}
+
+
+function BigButton() {
+    alert("sdhdushudssudhwu" + green);
+    green=green+1;
+    document.cookie = "green=" + green;
+}
+function change(){
+    document.getElementById("change").textContent = "chicken nuggets";
+    document.getElementById("change").style.backgroundColor = "red";
+    document.getElementById("change").style.padding = "20px";
+}
+function offload(){
+    document.getElementById("change").textContent = "Hover over me!";
+    document.getElementById("change").style.backgroundColor = "initial";
+    document.getElementById("change").style.padding = "initial";
+}
 new Swiper("#swiper-1", {
 
     effect: "fade", //cube, fade, coverflow, flip
